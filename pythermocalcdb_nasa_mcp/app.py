74a235fc40f209c8cc417b336511374a6d4c9528
sina.gilassi <sina.gilassi@gmail.com>
2026-08-18 00:05:21 -0400

    Refactor app.py: simplify main execution by removing MCP server instantiation

--- pythermocalcdb_nasa_mcp/app.py
+++ pythermocalcdb_nasa_mcp/app.py
@@ -1,11 +1,4 @@
-# import libs
+from pythermocalcdb_nasa_mcp.server import main
 
-# locals
-from pythermocalcdb_nasa_mcp.server import create_mcp_server
-
-# NOTE: build mcp
-mcp_server = create_mcp_server()
-
-# NOTE: run mcp server (stdio)
 if __name__ == "__main__":
-    mcp_server.run()
+    main()
