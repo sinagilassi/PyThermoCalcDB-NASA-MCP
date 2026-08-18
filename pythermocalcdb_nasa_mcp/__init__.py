9e42fd15eb8bad3c4448a2cacf24713b7f8411e3
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-08-18 17:55:33 -0400

    Add configuration imports and define module exports

---
+++ pythermocalcdb_nasa_mcp/__init__.py
@@ -0,0 +1,14 @@
+# NOTE: configs
+from .configs.info import (
+    __version__,
+    __description__,
+    __author__,
+    __package__
+)
+
+__all__ = [
+    "__version__",
+    "__description__",
+    "__author__",
+    "__package__"
+]
