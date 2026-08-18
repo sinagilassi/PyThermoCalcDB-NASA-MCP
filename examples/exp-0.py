4d8de3cf0fccfc7fc4b1ec6f54e53cebde6b8197
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-08-18 17:57:13 -0400

    Add example script to display library version and description

---
+++ examples/exp-0.py
@@ -0,0 +1,8 @@
+# import libs
+import pythermocalcdb_nasa_mcp
+
+# version
+print(
+    f"PyThermoCalcDB-NASA-MCP version: {pythermocalcdb_nasa_mcp.__version__}")
+# description
+print(f"Description: {pythermocalcdb_nasa_mcp.__description__}")
