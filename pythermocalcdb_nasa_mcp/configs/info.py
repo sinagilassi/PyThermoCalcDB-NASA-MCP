2cf5e479274779c24049c7449bf56fe55b5b125f
sina.gilassi <sina.gilassi@gmail.com>
2026-08-18 00:05:07 -0400

    Update package description for clarity and completeness

--- pythermocalcdb_nasa_mcp/configs/info.py
+++ pythermocalcdb_nasa_mcp/configs/info.py
@@ -6,6 +6,6 @@ __package__ = 'pythermocalcdb-nasa-mcp'
 # version
 __version__ = '0.1.0'
 # description
-__description__ = ''
+__description__ = 'MCP server and orchestration layer for pythermocalcdb-nasa thermodynamic calculations.'
 # author
 __author__ = 'Sina Gilassi'
